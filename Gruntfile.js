ce968ba3f8ce61b1f8d3affdbcaed6a0d75fb2e8
blackknight75 <devbox4714@gmail.com>
2017-01-23 01:58:03 -0700

    Added images to index

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
@@ -14,10 +13,8 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
   grunt.registerTask('default', ['uglify']);
 
 };
